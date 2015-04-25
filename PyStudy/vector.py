0a6f8ad765376403f737ad6271881cc5014dda72
HengLin <hlin@quixey.com>
2015-04-25 23:14:32 +0800

    new file

---
+++ PyStudy/vector.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env pyhon
+# -*- coding: utf-8 -*-
+
+class Vector:
+	""" Represent a vector in a multidimensional space. """
+
+	def __init__(self, d):
+		""" Create d-dimensional vector of zeros. """
+		self._coords = [0] * d
+
+	def __len__(self):
+		""" Return the dimension of the vector. """
+		return len(self._coords)
+
+	def __getitem__(self, j):
+		""" Return jth coordinate of vector. """
+		return self._coords[j]
+
+	def __setitem__(self, j, val):
+		""" Set jth coordinate of vector to given value."""
+		self._coords[j] = val
+
+	def __add__(self, other):
+		""" Return sum of two vectors. """
+		if len(self) != len(other):
+			raise ValueError('dimensions must agree')
+		result = Vector(len(self))
+		for j in range(len(self)):
+			result[j] = self[j] + other[j]
+		return result
+
+	def __eq__(self, other):
+		""" Return True if vector has some coordinate as other. """
+		return self._coords == other._coords
+
+	def __ne__(self, other):
+		""" Return True if vector differs from other. """
+		return not self == other
+
+	def __str__(self):
+		""" Produce string representation of vector. """
+		return '<' + str(self._coords)[1:-1] + '>'
+
+if __name__ == '__main__':
+	v1 = Vector(2)
+	v2 = Vector(3)
+	print len(v1) 
+	print len(v2)
